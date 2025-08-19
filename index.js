5065bb29955122b64571f3eb8f5ba32ceaf5c764
yeahpin96 <yeahpin96@gmail.com>
2025-08-19 18:20:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,61 +1,49 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const core = require("@actions/core");
-const { IncomingWebhook } = require("@slack/webhook");
-const axios = require("axios").default || require("axios");
+const webhook_1 = require("@slack/webhook");
+const axios_1 = require("axios");
 const qs = require("querystring");
-
 (async () => {
-  // --- Required inputs ---
-  const [productId, scheduleId, webhookUrl] = [
-    "product-id",
-    "schedule-id",
-    "slack-incoming-webhook-url",
-  ].map((name) => {
-    const value = core.getInput(name);
-    if (!value) {
-      throw new Error(`melon-ticket-actions: Please set ${name} input parameter`);
+    var _a;
+    // Validate parameters
+    const [productId, scheduleId, seatId, webhookUrl] = [
+        "product-id",
+        "schedule-id",
+        "seat-id",
+        "slack-incoming-webhook-url",
+    ].map((name) => {
+        const value = core.getInput(name);
+        if (!value) {
+            throw new Error(`melon-ticket-actions: Please set ${name} input parameter`);
+        }
+        return value;
+    });
+    const message = (_a = core.getInput("message")) !== null && _a !== void 0 ? _a : "티켓사세요";
+    const webhook = new webhook_1.IncomingWebhook(webhookUrl);
+    const res = await axios_1.default({
+        method: "POST",
+        url: "https://ticket.melon.com/tktapi/product/seatStateInfo.json",
+        params: {
+            v: "1",
+        },
+        data: qs.stringify({
+            prodId: productId,
+            scheduleNo: scheduleId,
+            seatId,
+            volume: 1,
+            selectedGradeVolume: 1,
+        }),
+    });
+    // tslint:disable-next-line
+    console.log("Got response: ", res.data);
+    if (res.data.chkResult) {
+        const link = `http://ticket.melon.com/performance/index.htm?${qs.stringify({
+            prodId: productId,
+        })}`;
+        await webhook.send(`${message} ${link}`);
     }
-    return value;
-  });
-
-  // --- Either block-id or seat-id must be provided (block-id takes precedence) ---
-  const blockId = core.getInput("block-id");
-  const seatId = core.getInput("seat-id");
-  if (!blockId && !seatId) {
-    throw new Error("melon-ticket-actions: Please set 'block-id' or 'seat-id' input parameter");
-  }
-  if (blockId && seatId) {
-    core.warning("Both 'block-id' and 'seat-id' were provided. 'block-id' will take precedence.");
-  }
-  const id = blockId || seatId;
-
-  const message = core.getInput("message") ?? "티켓사세요";
-  const webhook = new IncomingWebhook(webhookUrl);
-
-  const res = await axios({
-    method: "POST",
-    url: "https://ticket.melon.com/tktapi/product/seatStateInfo.json",
-    params: { v: "1" },
-    data: qs.stringify({
-      prodId: productId,
-      scheduleNo: scheduleId,
-      seatId: id,                   // ← 統一用 id；若是 block-id 也會傳這個值
-      volume: 1,
-      selectedGradeVolume: 1,
-    }),
-  });
-
-  // tslint:disable-next-line
-  console.log("Got response: ", res.data);
-
-  if (res.data.chkResult) {
-    const link = `http://ticket.melon.com/performance/index.htm?${qs.stringify({
-      prodId: productId,
-    })}`;
-    await webhook.send(`${message} ${link}`);
-  }
 })().catch((e) => {
-  console.error(e.stack); // tslint:disable-line
-  core.setFailed(e.message);
+    console.error(e.stack); // tslint:disable-line
+    core.setFailed(e.message);
 });
